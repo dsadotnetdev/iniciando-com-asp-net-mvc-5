8649907578fa3f23c42c4aedb468bb429276c44b
Daniel Sena <dsawebdev@hotmail.com>
2022-07-08 09:01:38 -0300

    Adiciona exemplo da aula "Action Results"

--- TesteMVC5/TesteMVC5/Controllers/HomeController.cs
+++ TesteMVC5/TesteMVC5/Controllers/HomeController.cs
@@ -1,30 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace TesteMVC5.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ViewResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ContentResult ContentResult()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            return Content("Olá");
         }
 
-        public ActionResult Contact()
+        public FileContentResult FileContentResult()
         {
-            ViewBag.Message = "Your contact page.";
+            var foto = System.IO.File.ReadAllBytes(Server.MapPath("/content/images/capa.png"));
+            return File(foto, "image/png", "capa.png");
+        }
 
-            return View();
+        public HttpUnauthorizedResult HttpUnauthorizedResult()
+        {
+            return new HttpUnauthorizedResult();
+        }
+
+        public JsonResult JsonResult()
+        {
+            return Json("teste: 'teste'", JsonRequestBehavior.AllowGet);
+        }
+
+        public RedirectResult RedirectResult()
+        {
+            return new RedirectResult("https://desenvolvedor.io");
+        }
+
+        public RedirectToRouteResult RedirectToRouteResult()
+        {
+            return RedirectToRoute(new { controller = "Home", action = "Index" });
+        }
+
+        public RedirectToRouteResult RedirectToRouteResult2()
+        {
+            return RedirectToAction("IndexTeste", "Teste");
         }
     }
 }
---
+++ TesteMVC5/TesteMVC5/Controllers/TesteController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace TesteMVC5.Controllers
+{
+    public class TesteController : Controller
+    {
+        public ViewResult IndexTeste()
+        {
+            return View();
+        }
+    }
+}
