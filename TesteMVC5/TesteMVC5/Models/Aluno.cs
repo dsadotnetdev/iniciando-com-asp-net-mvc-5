da6b80666a3689411dfdc9e4ff7de9d12e7405b4
Daniel Sena <dsawebdev@hotmail.com>
2022-07-08 11:45:14 -0300

    Adiciona exemplo da aula "O que é uma model?"

---
+++ TesteMVC5/TesteMVC5/Models/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TesteMVC5.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string CPF { get; set; }
+
+        public DateTime DataMatricula { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
