56730909411dcdd120a798a53d8f7aff177b9b2a
Daniel Sena <dsawebdev@hotmail.com>
2022-07-12 14:35:11 -0300

    Adiciona exemplo da aula "Configurando o EF na aplicação"

---
+++ TesteMVC5/TesteMVC5/Migrations/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity.Migrations;
+using TesteMVC5.Data;
+
+namespace TesteMVC5.Migrations
+{
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AppDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(TesteMVC5.Data.AppDbContext context)
+        {
+
+        }
+    }
+}
